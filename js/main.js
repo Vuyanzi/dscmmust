22e23335de09b94b2f19c3602e03aad22bda5133
Vuyanzi <vuyanziglorious@gmail.com>
2020-02-09 06:38:43 +0300

    added footer section

--- js/main.js
+++ js/main.js
@@ -6,3 +6,17 @@ function toggleMenu() {
         menu.classList.add("is-active");
     }
   }
+
+  $(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $('#scroll').fadeIn();
+        } else {
+            $('#scroll').fadeOut();
+        }
+    });
+    $('#scroll').click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 600);
+        return false;
+    });
+});
